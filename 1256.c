2e13d092a33086e148f18ad7097dc72630baabc1
david geraldi <david.geraldi@unifesp.br>
2025-11-25 23:09:55 -0300

    Exercicios adicionados

---
+++ 1256.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int M, C;
+        scanf("%d %d", &M, &C);
+
+        int tabela[1000][1000];
+        int tamanho[1000] = {0};
+
+        for (int i = 0; i < M; i++)
+            tamanho[i] = 0;    
+
+        for (int i = 0; i < C; i++) {
+            int x;
+            scanf("%d", &x);
+
+            int h = x % M;
+
+            tabela[h][tamanho[h]++] = x;
+        }
+
+        for (int i = 0; i < M; i++) {
+            printf("%d ->", i);
+            for (int j = 0; j < tamanho[i]; j++) {
+                printf(" %d ->", tabela[i][j]);
+            }
+            printf(" \\\n");
+        }
+
+        if (T > 0)
+            printf("\n");
+    }
+
+    return 0;
+}
